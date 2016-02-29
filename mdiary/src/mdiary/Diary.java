19a212021c36a7985f507938cff252157cd786cf
mina2289 <minapeng2289@gmail.com>
2016-02-28 21:39:54 -0800

    Added files via upload
    - Set up JDBC
    - Retrieve date from mysql

--- mdiary/src/mdiary/Diary.java
+++ mdiary/src/mdiary/Diary.java
@@ -3,17 +3,26 @@ package mdiary;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Diary {
-	public ArrayList<String> strDiary = new ArrayList<String>();
-	public Diary(){
-		
+	public Diary() {
+		Database db = new Database();
+		getDiarydb(db.getconn()); //debug
+		db.closeconn();
 	}
 	
-	void getInput(){
+	/*
+	 * Get diary input.
+	 */
+	void getInput() {
+		ArrayList<String> strDiary = new ArrayList<String>();
 		SimpleDateFormat formatDate = new SimpleDateFormat("yyyy/MM/dd E 'at' hh:mm:ss a zzz");
 		
 		System.out.println("Mina's Diary " + "\n" + formatDate.format(new Date()));
@@ -34,5 +43,46 @@ public class Diary {
 		}catch(IOException io){
 			io.printStackTrace();
 		}	
+	}
+	public void saveDiarydb(Connection conndb, ArrayList<String> strDiary) {
+		
+		
+	}
+	
+	
+	/*
+	 * Retrieve data from database
+	 */
+	public void getDiarydb(Connection conndb) {
+		Statement stmt = null; 
+	    try {
+			stmt = conndb.createStatement();
+		
+			// Set up query to database
+		    String query;
+		    query = "SELECT id, txt, date FROM testtable";
+		    ResultSet sqlResult = stmt.executeQuery(query);
+		
+		    // Extract data from the query result
+		    while(sqlResult.next()){
+		       // Retrieve by column name
+		       int id  = sqlResult.getInt("id");
+		       String txt = sqlResult.getString("txt");
+		       String date = sqlResult.getString("date");
+		
+		       // Display values
+		       System.out.println("ID: " + id);
+		       System.out.println("txt: " + txt);
+		       System.out.println("date: " + date + "\n");
+		    }
+		    // Clean-up environment
+		    sqlResult.close();
+		    stmt.close();
+		    conndb.close();
+	    } catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
 	}
 }
