d4e0a9a902133e2e38bd6f29b2f1642acef6592e
mina2289 <minapeng2289@gmail.com>
2016-03-02 21:20:28 -0800

    - Clean up classes - Create new db column - Can delete last entry in db

--- mdiary/src/mdiary/Diary.java
+++ mdiary/src/mdiary/Diary.java
@@ -3,10 +3,6 @@ package mdiary;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -43,82 +39,7 @@ public class Diary {
 		}	
 	}
 	
-	/*
-	 * Retrieve data from database
-	 */
-	public void getDiarydb(Connection conndb, String queryStr) {
-		Statement stmt = null;
-		ResultSet sqlResult = null;
-		try {
-			// Set up query to database
-	    	stmt = conndb.createStatement();
-		    sqlResult = stmt.executeQuery(queryStr);
-		
-		    // Extract data from the query result
-		    while(sqlResult.next()){
-		       // Retrieve by column name
-		       int id  = sqlResult.getInt("id");
-		       String txt = sqlResult.getString("txt");
-		       String date = sqlResult.getString("date");
-		
-		       // Display values
-		       System.out.println("ID: " + id);
-		       System.out.println("txt: " + txt);
-		       System.out.println("date: " + date + "\n");
-		    }
-		    
-	    } catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-		    try {
-		    	// Clean-up environment
-				sqlResult.close();
-				stmt.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		    
-		}
-	    
-	}
-	
-	/*
-	 * Insert data into database
-	 */
-	public void insertDiarydb(Connection conndb, String dbtable, int tableindex) {//, ArrayList<String> strDiary) {
-		Statement stmt = null;
-		
-		try {
-			// Insert data into database
-			stmt = conndb.createStatement();
-			StringBuilder txt = new StringBuilder();
-			for (String str: strDiary){
-				txt.append(str);
-			}
-			System.out.println(txt);
-			
-			//"yyyy-MM-dd E 'at' hh:mm:ss a zzz"
-			String insertStr = "INSERT INTO " +  dbtable + " VALUES ('" + ++tableindex + "', '" + txt.toString() 
-				+ "', '" + new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()) 
-					+ "')";
-		    
-			System.out.println(insertStr);
-		    stmt.executeUpdate(insertStr);
-		    
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-		    	// Clean-up environment
-				stmt.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
+	public ArrayList<String> getStrDiary(){
+		return strDiary;
 	}
 }
